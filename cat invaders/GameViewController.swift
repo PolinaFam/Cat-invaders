d1a324b36afab0889ffebc5b391c473cb9d80df1
PolinaFam <polinafamxxxx@gmail.com>
2020-05-04 18:03:38 +0700

    Created two screens: Menu and Game. Added classes for them.

--- cat invaders/GameViewController.swift
+++ cat invaders/GameViewController.swift
@@ -1,20 +1,26 @@
 //
-//  ViewController.swift
+//  GameViewController.swift
 //  cat invaders
 //
-//  Created by student on 03/05/2020.
+//  Created by student on 04/05/2020.
 //  Copyright © 2020 student. All rights reserved.
 //
 
 import UIKit
 
 class GameViewController: UIViewController {
-
+    
+    var score = 0
+    
+    @IBOutlet weak var scoreLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        updateScoreLabel()
     }
-
-
+    
+    func updateScoreLabel(){
+        scoreLabel.text = String(score)
+    }
 }
-
---
+++ cat invaders/MenuViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  cat invaders
+//
+//  Created by student on 03/05/2020.
+//  Copyright © 2020 student. All rights reserved.
+//
+
+import UIKit
+
+class MenuViewController: UIViewController {
+ 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
