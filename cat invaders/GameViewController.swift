a0308b2486dd44228e4f7362957c202805d405d7
PolinaFam <polinafamxxxx@gmail.com>
2020-05-04 21:03:58 +0700

    Added functions for moving spaceship.

--- cat invaders/GameViewController.swift
+++ cat invaders/GameViewController.swift
@@ -12,12 +12,37 @@ class GameViewController: UIViewController {
     
     var score = 0
     
+    @IBOutlet weak var spaceShip: UIImageView!
     @IBOutlet weak var scoreLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         updateScoreLabel()
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches {
+            let location = touch.location(in: view)
+            
+            if(location.x < view.frame.size.width/2){
+                moveShipToLeft(xCoordinates: location.x)
+            } else {
+                moveShipToRight(xCoordinates: location.x)
+            }
+        }
+    }
+    
+    func moveShipToLeft(xCoordinates: CGFloat){
+        if (spaceShip.center.x > 65) {
+            spaceShip.center.x -= 15
+        }
+    }
+    
+    func moveShipToRight(xCoordinates: CGFloat){
+        if (spaceShip.center.x < UIScreen.main.bounds.width - 65) {
+            spaceShip.center.x += 15
+        }
     }
     
     func updateScoreLabel(){
